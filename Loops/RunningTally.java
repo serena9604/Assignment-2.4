c2c90d8ae55d958cdb0581c5fbcbc2550333f7ee
Serena Bi <56049445+serena9604@users.noreply.github.com>
2019-10-03 13:30:25 -0700

    Add files via upload

---
+++ Loops/RunningTally.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class RunningTally
+{
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("RunningTally");
+        System.out.println("---------------------------------");
+        System.out.println("");
+        System.out.println("Let's add some numbers! [Type a negative number to quit]");
+        
+        int i = sc.nextInt();
+        int total = 0;
+        boolean add = true;
+        while (i >= 0 && add == true) {
+            System.out.println("Add: " + i);
+            total = total + i;
+            add = false;
+            i = sc.nextInt();
+        }
+        if (i < 0) {
+            System.out.print("Total: " + total);
+        }
+        
+        
+
+    }
+}
+
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
